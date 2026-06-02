c0fbd84585e49c04382a0779746ef49a7cba533d
Ganesh Arkanath <ganesharkanath15@gmail.com>
2026-06-01 23:12:44 -0700

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        q = collections.deque()
+        self.res = 0
+        self.count = 0
+
+        def bfs():
+            while q:
+                i, j, k = q.popleft()
+                if i < 0 or i == M or j < 0 or j == N or grid[i][j] != 1:
+                    continue
+                
+                grid[i][j] = 2
+                self.count -= 1
+                k += 1
+                self.res = max(self.res, k)
+                q.append((i+1,j,k))
+                q.append((i-1,j,k))
+                q.append((i,j+1,k))
+                q.append((i,j-1,k))
+
+        for i in range(M):
+            for j in range(N):
+                if grid[i][j] == 2:
+                    q.append((i+1,j,0))
+                    q.append((i-1,j,0))
+                    q.append((i,j+1,0))
+                    q.append((i,j-1,0))
+                elif grid[i][j] == 1:
+                    self.count += 1
+        bfs()
+
+        if self.count > 0:
+            return -1
+        return self.res
